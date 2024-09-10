1a28dbbd26da2ce90de7928f5e35003f346958d2
Jackson Blankenship <jacksblan@gmail.com>
2024-09-10 18:02:29 -0500

    improve RADARR_PORT validation

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -11,7 +11,12 @@ import {
 export const env = createEnv({
   server: {
     RADARR_BASE_URL: z.string().url(),
-    RADARR_PORT: z.coerce.number().min(0).default(DEFAULT_RADARR_PORT),
+    RADARR_PORT: z.coerce
+      .number()
+      .int()
+      .min(0)
+      .max(0xffff)
+      .default(DEFAULT_RADARR_PORT),
     RADARR_API_KEY: z.string().min(1),
 
     DISCORD_WEBHOOK_URL: z.string().url().optional(),
