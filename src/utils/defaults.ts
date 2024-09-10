c05362a06d93c78256a64d7627fb5a3adb847900
Jackson Blankenship <jacksblan@gmail.com>
2024-09-10 12:37:56 -0500

    prepare env vars for new schema

--- src/utils/defaults.ts
+++ src/utils/defaults.ts
@@ -1,4 +1,6 @@
-export const DEFAULT_MOVIE_EXPIRY_DAYS = 30;
-export const DEFAULT_DAYS_BEFORE_EXPIRY_NOTICE = 7;
 export const DEFAULT_CRON_SCHEDULE = '0 6 * * *';
 export const DEFAULT_LOG_LEVEL = 'info';
+export const DEFAULT_RADARR_PORT = 7878;
+
+export const DEFAULT_MOVIE_EXPIRY_DAYS = 30;
+export const DEFAULT_DAYS_BEFORE_EXPIRY_NOTICE = 5;
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -5,19 +5,23 @@ import {
   DEFAULT_DAYS_BEFORE_EXPIRY_NOTICE,
   DEFAULT_CRON_SCHEDULE,
   DEFAULT_LOG_LEVEL,
+  DEFAULT_RADARR_PORT,
 } from './defaults';
 
 export const env = createEnv({
   server: {
-    RADARR_URL: z.string().min(1),
+    RADARR_BASE_URL: z.string().url(),
+    RADARR_PORT: z.coerce.number().min(0).default(DEFAULT_RADARR_PORT),
     RADARR_API_KEY: z.string().min(1),
-    RADARR_MDBLIST_TAG_ID: z.coerce.number().min(0),
 
+    DISCORD_WEBHOOK_URL: z.string().url().optional(),
+
+    CRON_SCHEDULE: z.string().default(DEFAULT_CRON_SCHEDULE),
+    LOG_LEVEL: z.enum(['info', 'debug']).default(DEFAULT_LOG_LEVEL),
+
+    // TODO: Each of these variables should be removed as a part of this PR
     MDBLIST_USERNAME: z.string().min(1),
     MDBLIST_LIST_NAME: z.string().min(1),
-
-    DISCORD_WEBHOOK_URL: z.string().url(),
-
     MOVIE_EXPIRY_DAYS: z.coerce
       .number()
       .min(0)
@@ -26,9 +30,8 @@ export const env = createEnv({
       .number()
       .min(0)
       .default(DEFAULT_DAYS_BEFORE_EXPIRY_NOTICE),
-
-    CRON_SCHEDULE: z.string().default(DEFAULT_CRON_SCHEDULE),
-    LOG_LEVEL: z.enum(['info', 'debug']).default(DEFAULT_LOG_LEVEL),
+    RADARR_URL: z.string().min(1),
+    RADARR_MDBLIST_TAG_ID: z.coerce.number().min(0),
   },
   runtimeEnv: process.env,
 });
